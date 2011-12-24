af83c356880ca0c4ce4b75b63540476dcfa83d0a
Björn Edström <be@bjrn.se>
2011-12-24 23:48:04 +0100

    Initial commit of hand code.

---
+++ README.md
@@ -0,0 +1,10 @@
+pyker
+=====
+
+Python Poker code.
+
+This is really a companion to my haskell-holdem project. It may
+eventually turn into something useful. Right now I use it for
+sketching.
+
+- Björn
---
+++ pyker/poker.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2011 Björn Edström <be@bjrn.se>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the University nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+# TODO (bjorn): I'm not sure how it happened, but the code below
+# turned out to be too-god-d**n-object-oriented :-) Should perhaps
+# rewrite to be either more pythonic, or adapt OOP fully.
+
+class Rank(object):
+    def __init__(self, rank):
+        self.rank = rank
+
+    def __hash__(self):
+        return hash(self.rank)
+
+    def __cmp__(self, other):
+        return cmp(self.rank, other.rank)
+
+    def __str__(self):
+        return self.__RANKS[self.rank]
+
+    def pretty(self):
+        pretty_rank = {
+            14: 'Ace',
+            13: 'King',
+            12: 'Queen',
+            11: 'Jack',
+            10: 'Ten',
+            9: 'Nine',
+            8: 'Eight',
+            7: 'Seven',
+            6: 'Six',
+            5: 'Five',
+            4: 'Four',
+            3: 'Three',
+            2: 'Two',
+            }
+        return pretty_rank[self.rank]
+
+    def __repr__(self):
+        return '<Rank %s>' % (self.rank,)
+
+    __RANKS = 'xx23456789tjqka'
+
+    @staticmethod
+    def from_string(s):
+        r = Rank(None)
+        r.rank = Rank.__RANKS.index(s.lower())
+        return r
+
+class Card(object):
+    HEART = 'h'
+    SPADES = 's'
+    CLUBS = 'c'
+    DIAMONDS = 'd'
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    __RANKS = 'xx23456789tjqka'
+
+    def __str__(self):
+        return '%s%s' % (self.rank, self.suit)
+
+    def __repr__(self):
+        return '<Card rank=%r suit=%s>' % (self.rank, self.suit)
+
+    @staticmethod
+    def from_string(s):
+        rank_str, suit = s[0], s[1]
+        rank = Rank.from_string(rank_str)
+        return Card(rank, suit)
+
+class HandClass(object):
+    def score(self):
+        raise NotImplementedError('not implemented')
+
+    def __cmp__(self, other):
+        return cmp(self.score(), other.score())
+
+    def __repr__(self):
+        return '<%s %r>' % (self.__class__.__name__, self.__dict__)
+
+class StraightFlush(HandClass):
+    def __init__(self, highest):
+        self.highest = highest
+
+    def score(self):
+        return 8 * 13**5 + (self.highest.rank - 2)
+
+    def __str__(self):
+        return 'Straight Flush, %s High' % (self.highest.pretty(),)
+
+class FourOfAKind(HandClass):
+    def __init__(self, four, kicker):
+        self.four = four
+        self.kicker = kicker
+
+    def score(self):
+        return 7 * 13**5 + (self.four.rank - 2) * 13 + (self.kicker.rank - 2)
+
+    def __str__(self):
+        return 'Four of a Kind of %ss, kicker %s' % (self.four.pretty(), self.kicker.pretty())
+
+class FullHouse(HandClass):
+    def __init__(self, three, two):
+        self.three = three
+        self.two = two
+
+    def score(self):
+        return 6 * 13**5 + (self.three.rank - 2) * 13 + (self.two.rank - 2)
+
+    def __str__(self):
+        return 'Full House, 3 %ss and 2 %ss' % (self.three.pretty(), self.two.pretty())
+
+class Flush(HandClass):
+    def __init__(self, kickers):
+        self.kickers = kickers
+
+    def score(self):
+        score = 0
+        for i in xrange(len(self.kickers)):
+            score += (self.kickers[i].rank - 2) * 13**i
+        return 5 * 13**5 + score
+
+    def __str__(self):
+        return 'Flush with %s' % (', '.join(map(lambda s: s.pretty(), self.kickers)),)
+
+class Straight(HandClass):
+    def __init__(self, highest):
+        self.highest = highest
+
+    def score(self):
+        return 4 * 13**5 + (self.highest.rank - 2)
+
+    def __str__(self):
+        return 'Straight, %s High' % (self.highest.pretty(),)
+
+class ThreeOfAKind(HandClass):
+    def __init__(self, three, kickers):
+        self.three = three
+        self.kickers = kickers
+
+    def score(self):
+        return 3 * 13**5 + (self.three.rank - 2) * 13**2 + (self.kickers[0].rank - 2) * 13**1 + (self.kickers[1].rank - 2)
+
+    def __str__(self):
+        kickers = ', '.join(map(lambda s: s.pretty(), self.kickers))
+        return 'Three of a Kind of %ss with %s' % (self.three.pretty(), kickers)
+
+class TwoPair(HandClass):
+    def __init__(self, high_pair, low_pair, kicker):
+        self.high_pair = high_pair
+        self.low_pair = low_pair
+        self.kicker = kicker
+
+    def score(self):
+        return 2 * 13**5 + (self.high_pair.rank - 2) * 13**2 + (self.low_pair.rank - 2) * 13**1 + (self.kicker.rank - 2)
+
+    def __str__(self):
+        return 'Two Pairs of %ss and %ss, kicker %s' % (self.high_pair.pretty(), self.low_pair.pretty(), self.kicker.pretty())
+
+class Pair(HandClass):
+    def __init__(self, pair, kickers):
+        self.pair = pair
+        self.kickers = kickers
+
+    def score(self):
+        return 1 * 13**5 + (self.pair.rank - 2) * 13**3 + (self.kickers[0].rank - 2) * 13**1 + (self.kickers[1].rank - 2) * 13**1 + (self.kickers[2].rank - 2)
+
+    def __str__(self):
+        kickers = ', '.join(map(lambda s: s.pretty(), self.kickers))
+        return 'Pair of %ss with %s' % (self.pair.pretty(), kickers)
+
+class Highest(HandClass):
+    def __init__(self, kickers):
+        self.kickers = kickers
+
+    def score(self):
+        score = 0
+        for i in xrange(len(self.kickers)):
+            score += (self.kickers[i].rank - 2) * 13**i
+        return score
+
+    def __str__(self):
+        return 'Highest Cards %s' % (', '.join(map(lambda s: s.pretty(), self.kickers)),)
+
+class Hand(object):
+    def __init__(self, cards):
+        self.cards = cards
+
+    def classify(self):
+        """
+        """
+
+        assert len(self.cards) == 5
+
+        ranks = sorted((c.rank for c in self.cards))
+
+        # Highest card if straight
+        straight = None
+
+        # Special case when ace is low:
+        if ranks == [Rank(2), Rank(3), Rank(4), Rank(5), Rank(14)]:
+            straight = Rank(5)
+
+        # All the other ("normal") straights:
+        elif ranks == map(Rank, range(ranks[0].rank, ranks[4].rank + 1)):
+            straight = ranks[4]
+
+        # Is a flush?
+        flush = all((c.suit == self.cards[0].suit
+                     for c in self.cards))
+
+        # We now have enough info to determine three hand classes:
+        if straight is not None and flush:
+            return StraightFlush(straight)
+
+        if flush:
+            return Flush((ranks[4], ranks[3], ranks[2], ranks[1], ranks[0]))
+
+        if straight is not None:
+            return Straight(straight)
+
+        # Determine pairs, required for further classifications.
+        pairs = {}
+        for r in ranks:
+            try:
+                pairs[r] += 1
+            except:
+                pairs[r] = 1
+        has = {
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            }
+        for r, num in pairs.iteritems():
+            has[num].append(r)
+
+        if has[4]:
+            return FourOfAKind(max(has[4]), max(has[1]))
+
+        if has[3] and has[2]:
+            return FullHouse(max(has[3]), max(has[2]))
+
+        if has[3]:
+            return ThreeOfAKind(max(has[3]), (max(has[1]), min(has[1])))
+
+        if len(has[2]) == 2:
+            return TwoPair(max(has[2]), min(has[2]), max(has[1]))
+
+        if len(has[2]) == 1:
+            kick = sorted(has[1])
+            return Pair(max(has[2]), (kick[2], kick[1], kick[0]))
+
+        return Highest((ranks[4], ranks[3], ranks[2], ranks[1], ranks[0]))
+
+    @staticmethod
+    def from_string(s):
+        return Hand(map(Card.from_string, s.split()))
+
+    def __repr__(self):
+        return '<Hand %s>' % (' '.join(map(str, self.cards)))
+
+
+if __name__ == '__main__':
+    c1 = Card.from_string('9h')
+    c2 = Card.from_string('th')
+
+    hh = [
+        '7h 8h 9h th jh',
+        '7h 7s 7c 7d jh',
+        '3h 3c 3s tc th',
+        '7h 8h 2h th jh',
+        '9c 8h 7d td jh',
+        '3c 3h 3d td js',
+        '2c 2s 3d 3h jd',
+        '7c 4s 4d th jd',
+        'kc as 9d 3h 2d'
+    ]
+
+    
+    for h in hh:
+        #print h, repr(Hand.from_string(h).classify())
+        print h,
+        try:
+            print Hand.from_string(h) #.classify()
+        except Exception, e:
+            print 'failed', e
+
+
+
+
